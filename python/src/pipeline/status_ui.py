a30e755f47c0216757a99d98ec92066996a4fdbb
MattFaus <mattfaus@khanacademy.org>
2013-05-09 16:02:56 -0700

    Fix /mapreduce/pipeline to properly ZipImport
    Test Plan:
    Craig's review
    (ZipImport's only happen in production, so hard to test locally)
    
    Reviewers: csilvers
    
    Differential Revision: http://phabricator.khanacademy.org/D2281

--- python/src/pipeline/status_ui.py
+++ python/src/pipeline/status_ui.py
@@ -96,10 +96,10 @@ class _StatusUiHandler(webapp.RequestHandler):
 
     # It's possible we're inside a zipfile (zipimport).  If so,
     # __file__ will start with 'something.zip'.
-    (possible_zipfile, zip_path) = os.path.relpath(path).split(os.sep, 1)
-    try:
-      content = zipfile.ZipFile(possible_zipfile).read(zip_path)
-    except (IOError, OSError, zipfile.BadZipfile):
+    if ('.zip' + os.sep) in path:
+      (zip_file, zip_path) = os.path.relpath(path).split('.zip' + os.sep, 1)
+      content = zipfile.ZipFile(zip_file + '.zip').read(zip_path)
+    else:
       try:
         content = pkgutil.get_data(__name__, relative_path)
       except AttributeError:  # Python < 2.6.
